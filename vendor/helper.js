ecfc03c545118e3347befbfa1a0781aa3090d998
郭漫丽 <404544332@qq.com>
2017-10-07 01:05:15 +0800

    增加对应obj

--- vendor/helper.js
+++ vendor/helper.js
@@ -11,7 +11,22 @@ var consoleAsnyc = function(val,callback) {
   }, 1);
 }
 
+var consoleFuncForObj = function(val,key,callback) {
+  console.log('进入键为' + key + '值为' + val + '方法');
+  callback(null,val);
+};
+
+var consoleAsnycForObj = function(val,key,callback) {
+  console.log('键为' + key + '值为' + val);
+  setTimeout(function() {
+    console.log('进入键为' + key + '值为' + val + '方法');
+    callback(null,val);
+  }, 1);
+}
+
 module.exports = {
   consoleFunc,
   consoleAsnyc,
+  consoleFuncForObj,
+  consoleAsnycForObj
 };
